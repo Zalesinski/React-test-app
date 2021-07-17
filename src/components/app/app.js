1dddffee3be796e69ae45427a35c30ed0c283a91
Zalesinski <zalesinsky@gmail.com>
2021-07-17 11:59:23 +0300

    All features ready
    Added:
    
    - search
    - filter
    - delete function
    - important and like buttons

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,21 +8,40 @@ import PostAddForm from '../post-add-form';
 
 import './app.css';
 
+let ids = [1, 2, 3];
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getId() {
+    let id = random(1, 9999);
+    while (ids.includes(id)) {
+        id = random(1, 9999);
+    }
+    ids.push(id);
+    return id;
+}
+
 export default class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             data : [
-                {label: 'Going to learn React', important: true, id: 1},
-                {label: 'Need some rest...', important: false, id: 2},
-                {label: 'Working from home', important: false, id: 3}
-            ]
+                {label: 'Going to learn React', important: true, like: true, id: 1},
+                {label: 'Need some rest...', important: false, like: false, id: 2},
+                {label: 'Working from home', important: false, like: false, id: 3}
+            ],
+            term: '',
+            filter: 'all'
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
-
-        this.maxId = 4;
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
+ 
     }
 
     deleteItem(id) {
@@ -43,7 +62,7 @@ export default class App extends Component {
         const newItem = {
             label: body,
             important: false,
-            id: this.maxId++
+            id: getId()
         }
 
         this.setState(({data}) => {
@@ -54,17 +73,82 @@ export default class App extends Component {
         });
     }
 
+    toggle(id, item) {
+        this.setState(({data}) => {
+            const index =  data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, [item]: !old[item]};
+
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+    onToggleLiked(id) {
+        this.toggle(id, 'like')
+    }
+
+    onToggleImportant(id) {
+        this.toggle(id, 'important')
+    }
+
+    searchPost(items, term) {
+        if(term.length === 0) {
+            return items;
+        }
+
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1
+        });
+    }
+
+    filterPost(items, filter) {
+        if (filter === 'like') {
+            return items.filter(item => item.like);
+        } else {
+            return items;
+        }
+    }
+
+    onUpdateSearch(term) {
+        this.setState({term});
+    }
+
+    onFilterSelect(filter) {
+        this.setState({filter});
+    }
+
     render() {
+        const {data, term, filter} = this.state;
+
+        const allPosts = data.length;
+        const liked = data.filter(item => item.like).length;
+        
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
+
         return (
             <div className="app">
-                 <AppHeader/>
+                 <AppHeader
+                 liked={liked}
+                 allPosts={allPosts}
+                 />
                  <div className="search-panel d-flex">
-                    <SearchPanel/>
-                    <PostStatusFilter/>
+                    <SearchPanel
+                    onUpdateSearch={this.onUpdateSearch}
+                    />
+                    <PostStatusFilter
+                    filter={filter}
+                    onFilterSelect={this.onFilterSelect}
+                    />
                  </div>
                  <PostList 
-                    posts={this.state.data} 
-                    onDelete={ this.deleteItem}/>
+                    posts={visiblePosts} 
+                    onDelete={this.deleteItem}
+                    onToggleImportant={this.onToggleImportant}
+                    onToggleLiked={this.onToggleLiked}/>
                  <PostAddForm
                     onAdd={this.addItem}/>
             </div>
